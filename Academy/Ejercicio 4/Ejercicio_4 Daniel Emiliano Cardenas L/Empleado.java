889815d1a18d654ccb69ef205e216eb52585fb58
Daniel Cardenas <daniel.cardenas1767@alumnos.udg.mx>
2025-11-20 12:45:36 -0600

    Add files via upload

---
+++ Academy/Ejercicio 4/Ejercicio_4 Daniel Emiliano Cardenas L/Empleado.java
@@ -0,0 +1,32 @@
+public class Empleado {
+    private int id;
+    private String nombre;
+    private double salario;
+
+    public Empleado(int id, String nombre, double salario) {
+        this.id = id;
+        this.nombre = nombre;
+        this.salario = salario;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    @Override
+    public String toString() {
+        return "Empleado{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", salario=" + salario +
+                '}';
+    }
+}
